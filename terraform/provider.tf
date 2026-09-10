d1bc89b203f8a489b8f42eade8f9082f976e1918
Shadow-DeV9 <flinx08@icloud.com>
2026-09-10 14:07:58 +0100

    tf provider update

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -5,6 +5,12 @@ terraform {
       version = "~> 6.0"
     }
   }
+
+  backend "s3" {
+    bucket = "ecs-project-terraform-state-343918153740"
+    key    = "terraform.tfstate"
+    region = "eu-north-1"
+  }
 }
 
 # Configure the AWS Provider
